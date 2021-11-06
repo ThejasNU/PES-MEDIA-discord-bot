1e42037e30ca7e1ad10198d0bcaafa82fda97a87
Bhagyashree-hegde2001 <82833267+Bhagyashree-hegde2001@users.noreply.github.com>
2021-11-06 07:39:03 +0530

    Add files via upload

---
+++ commands/god.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "god",
+	description: "Display god of the server!",
+	async execute(message) {
+		await message.channel.send(`That is <@676050443180179468>`);
+	},
+};
---
+++ commands/topper.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "topper",
+	description: "Display topper in the server!",
+	async execute(message) {
+		await message.channel.send(`That is <@708719821226901534>`);
+	},
+};
