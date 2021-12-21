ee1e3e52e4818e76d39de300c2551e9058a02b52
Thejas N U <77475353+ThejasNU@users.noreply.github.com>
2021-12-21 15:53:34 +0530

    made it to take big content

--- commands/spam.js
+++ commands/spam.js
@@ -2,23 +2,27 @@ module.exports = {
 	name: "spam",
 	description: "spams with the number and content given",
 	async execute(message, args) {
-    // const userID = ['<@793111567184297985>','<@760161883336081408>','<@764118123330273330>'];
 		if (message.author.id === '793111567184297985' || message.author.id === '760161883336081408' || message.author.id === '764118123330273330') {
-            let num = args[1];
-            let content = args[0];
-            if(!args[0] || !args[1] ){
-              await message.channel.send("Give proper arguments Like this");
-               await message.channel.send("`$spam content number`");
+            let len=args.length;
+            let num = args[len-1];
+            let content = "";
+            for (let i = 0; i < (len-1); i++) {
+			    content = content + " " + args[i];
+	    	}
+            if(isNaN(args[len-1])){
+                await message.channel.send("Give proper arguments Like this");
+                await message.channel.send("`$spam content number`");
+                await message.channel.send("Make sure that last argument is **number**")
             }
-            else {
-            for(var i = 0;i < num;i++){
-                await message.channel.send(content);
+            else{
+                num=Number(num)
+                for(let i = 0;i < num;i++){
+                    await message.channel.send(content);
+                }
             }
-            }
-			
 		} else {
 			await message.channel.send(
-				"You don't have permission to use this command");
+				"Hehe, You don't have permission to use this command");
 		}
 	}
 };
